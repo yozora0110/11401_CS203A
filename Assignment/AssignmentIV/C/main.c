9572a20268ded689e62039d26a6a96f926ba3a79
Yozora <nightsky0110@gmail.com>
2025-11-24 04:22:00 +0800

    Update main.c

--- Assignment/AssignmentIV/C/main.c
+++ Assignment/AssignmentIV/C/main.c
@@ -1,45 +1,63 @@
+/* 
+   ========================================
+   Hash Function Observation (C Version)
+   ========================================
+   
+   Description:
+   This program tests and displays the output of hash functions for integer
+   and string keys using different table sizes. It utilizes the hash functions
+   defined in "hash_fn.c".
+
+   Development History:
+    - 2025/11/11: Initial implementation
+
+   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+ */
+
 #include <stdio.h>
+#include <string.h>
 #include "hash_fn.h"
 
-void test_int_hash() {
-    printf("=== Testing Integer Hash Functions ===\n");
-    
-    int keys[] = {10, 25, 37, 42, 58, 63, 79, 84, 91, 105};
-    int m = 10; // 哈希表大小
-    
-    printf("Key\tDivision\tMultiplication\n");
-    printf("---\t--------\t-------------\n");
-    
-    for (int i = 0; i < 10; i++) {
-        int div_hash = hash_int_division(keys[i], m);
-        int mult_hash = hash_int_multiplication(keys[i], m);
-        printf("%d\t%d\t\t%d\n", keys[i], div_hash, mult_hash);
-    }
-    printf("\n");
-}
+// Caution: Do not change the content of this file.
 
-void test_string_hash() {
-    printf("=== Testing String Hash Functions ===\n");
-    
-    const char* strings[] = {"hello", "world", "hash", "function", "test"};
-    int m = 10;
-    
-    printf("String\t\tSimple\tDJB2\n");
-    printf("------\t\t------\t----\n");
-    
-    for (int i = 0; i < 5; i++) {
-        int simple_hash = hash_string_simple(strings[i], m);
-        int djb2_hash = hash_string_djb2(strings[i], m);
-        printf("%s\t\t%d\t%d\n", strings[i], simple_hash, djb2_hash);
-    }
-    printf("\n");
-}
+int main(void) {
+    // Integer keys
+    int int_keys[] = {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60};
+    int num_keys = sizeof(int_keys) / sizeof(int_keys[0]);
 
-int main() {
-    printf("Hash Function Testing Program\n\n");
-    
-    test_int_hash();
-    test_string_hash();
-    
+    // Table sizes to test
+    int table_sizes[] = {10, 11, 37};
+    int num_sizes = sizeof(table_sizes) / sizeof(table_sizes[0]);
+
+    printf("=== Hash Function Observation (C Version) ===\n\n");
+
+    for (int s = 0; s < num_sizes; s++) {
+        int m = table_sizes[s];
+        printf("=== Table Size m = %d ===\n", m);
+        printf("Key\tIndex\n");
+        printf("-----------------\n");
+        for (int i = 0; i < num_keys; i++) {
+            int idx = myHashInt(int_keys[i], m);
+            printf("%d\t%d\n", int_keys[i], idx);
+        }
+        printf("\n");
+    }
+
+    // String keys (Non-integer keys)
+    const char* str_keys[] = {"cat", "dog", "bat", "cow", "ant", "owl", "bee", "hen", "pig", "fox"};
+    num_keys = sizeof(str_keys) / sizeof(str_keys[0]);
+
+    for (int s = 0; s < num_sizes; s++) {
+        int m = table_sizes[s];
+        printf("=== String Hash (m = %d) ===\n", m);
+        printf("Key\tIndex\n");
+        printf("-----------------\n");
+        for (int i = 0; i < num_keys; i++) {
+            int idx = myHashString(str_keys[i], m);
+            printf("%s\t%d\n", str_keys[i], idx);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
